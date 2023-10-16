8aa7318802ce4d55e661edfd6b9144d4ccfdae26
ZoryaGs <103145887+ZoryaGs@users.noreply.github.com>
2023-10-17 02:03:57 +0300

    app fixes

--- RosMolApp/App.xaml.cs
+++ RosMolApp/App.xaml.cs
@@ -7,7 +7,7 @@ public partial class App : Application
 	public App()
 	{
         Application.Current.UserAppTheme = AppTheme.Light;
-
+        
 		InitializeComponent();
 
 		MainPage = new AppShell();
